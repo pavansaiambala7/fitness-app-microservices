22c153dd56bf975468b6528cc59a1e65ed3bf79f
Pavan Sai Ambala <pavansaiambala7p@gmail.com>
2026-08-13 15:15:38 +0530

    test(activity): enhance context load unit tests for Activity Service

--- activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.fitness.activityservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = ActivityserviceApplication.class)
 class ActivityserviceApplicationTests {
 
 	@Test
+	@DisplayName("Activity Service spring context loads successfully")
 	void contextLoads() {
+		assertTrue(true, "Activity Service spring context initialized");
 	}
-
 }
+
