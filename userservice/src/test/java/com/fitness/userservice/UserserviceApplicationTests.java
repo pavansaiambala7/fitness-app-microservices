cd95140238224075dff65cf5a1d714b8355765f7
Pavan Sai Ambala <pavansaiambala7p@gmail.com>
2026-08-13 15:15:19 +0530

    test(user): enhance context load unit tests for User Service

--- userservice/src/test/java/com/fitness/userservice/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/fitness/userservice/UserserviceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.fitness.userservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = UserserviceApplication.class)
 class UserserviceApplicationTests {
 
 	@Test
+	@DisplayName("User Service spring context loads successfully")
 	void contextLoads() {
+		assertTrue(true, "User Service spring context initialized");
 	}
-
 }
+
